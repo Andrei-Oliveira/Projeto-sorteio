99da80ebc93e9bdb975bd6f0ccf95b1f86f46db4
Andrei <andrei.oliveira_@hotmail.com>
2023-06-27 18:10:34 -0300

    Projeto-fanpage-sorteador

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function generateNumber (){
+
+
+const min= Math.ceil(document.querySelector (".inputOne").value)
+const max=Math.floor(document .querySelector (".inputTwo").value)
+const result=  Math.floor(Math.random() * (max - min +1) + min)
+console.log ( min,max)
+alert (result)
+}
+
